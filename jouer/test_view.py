3662f6be10b6563116120813dfa92492d3c3b8ce
remy385 <remy.gaboriau@gmail.com>
2023-03-07 10:12:40 +0100

    test

--- jouer/test_view.py
+++ jouer/test_view.py
@@ -3,5 +3,10 @@ from django.urls import reverse
 
 class indexTest(TestCase):
     def test_view_url_exists_at_desired_location(self):
-        response = self.client.get('/jouer')
+        response = self.client.get('/jouer/')
         self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get('/jouer/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'jouer/templates/home.html')
