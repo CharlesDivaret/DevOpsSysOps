eacb2e57032b926846eca64be2dfeeabc1c3b6ad
divar <divaret.c@gmail.com>
2023-03-07 09:51:00 +0100

    tirage mot aléa dans une list

--- jouer/views.py
+++ jouer/views.py
@@ -1,5 +1,33 @@
+from shlex import join
+
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    import random
+
+    # Liste de mots
+    mots = ['chat', 'chien', 'oiseau', 'poisson', 'lapin', 'tortue', 'souris', 'cheval', 'cochon', 'vache', 'mouton',
+            'poulet', 'canard', 'oie', 'cygne', 'abeille', 'fourmi', 'araignée', 'guêpe', 'sauterelle', 'criquet',
+            'scarabée', 'papillon', 'chenille', 'limace', 'escargot', 'serpent', 'lézard', 'grenouille', 'têtard',
+            'crocodile', 'alligator', 'ours', 'loup', 'renard', 'raton laveur', 'blaireau', 'hermine', 'chauve-souris',
+            'écureuil', 'castor', 'rat', 'furet', 'opossum', 'lynx', 'tigre', 'lion', 'léopard', 'panthère', 'jaguar',
+            'guépard', 'éléphant', 'rhinocéros', 'hippopotame', 'girafe', 'chameau', 'dromadaire', 'kangourou', 'koala',
+            'singe', 'gorille', 'chimpanzé', 'orang-outan', 'phoque', 'otarie', 'marsouin', 'dauphin', 'baleine',
+            'requin', 'méduse', 'étoile de mer', 'crabe', 'homard', 'crevette', 'moule', 'huître', 'escargot de mer',
+            'anguille', 'anguillon', 'truite', 'saumon', 'requin', 'morue', 'sole', 'carpe', 'brochet', 'perche',
+            'grondin', 'rascasse', 'merlan', 'bar', 'sardine', 'hareng', 'anchois', 'thon', 'maquereau', 'crevette',
+            'langoustine', 'homard', 'crabe', 'calamar', 'poulpe', 'méduse', 'étoile de mer', 'oursin', 'corail',
+            'algue', 'fleur', 'arbre', 'herbe', 'fruit', 'légume', 'pain', 'viande', 'poisson', 'fromage', 'oeuf',
+            'lait', 'eau', 'vin', 'bière', 'café', 'thé', 'chocolat', 'bonbon', 'gâteau', 'crème glacée', 'pizza',
+            'hamburger', 'frites', 'salade', 'soupe', 'riz', 'nouilles', 'pâtes', 'pain', 'beurre', 'huile', 'sel',
+            'poivre', 'sucre']
+
+    # Liste aléatoire de 100 mots
+    mot = ''+join(random.sample(mots, 1))
+    motHidden=""
+    for i in range (len(mot)):
+        motHidden+="*"
+    print(motHidden)
+    return render(request, 'jouer/home.html',{'mot': mot, 'motHidden' : motHidden})
--- pendu/settings.py
+++ pendu/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'jouer.apps.JouerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'jouer'
 ]
 
 MIDDLEWARE = [
